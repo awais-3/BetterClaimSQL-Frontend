02f1ae307d671672a1c998263bd381b48e63ba76
Awais Khan <awais@contrivers.dev>
2024-12-29 23:30:20 +0500

    Fix backedn url issue

--- src/api/claimTransactions.ts
+++ src/api/claimTransactions.ts
@@ -1,44 +1,45 @@
-
-
-
-const API_URL = ""
+const API_URL = import.meta.env.VITE_API_URL;
 //const API_URL = "http://localhost:5001"
 
 export async function getClaimTransactionsInfo() {
-    const url  = `${API_URL}/api/claim-transactions/info`
-    const claimTransactions = await fetch(url)
-    return await claimTransactions.json()
+  const url = `${API_URL}api/claim-transactions/info`;
+  const claimTransactions = await fetch(url);
+  return await claimTransactions.json();
 }
-
 
 export async function getClaimTransactions() {
-    const url  = `${API_URL}/api/claim-transactions/`
-    const claimTransactions = await fetch(url)
-    return await claimTransactions.json()
+  const url = `${API_URL}api/claim-transactions/`;
+  const claimTransactions = await fetch(url);
+  return await claimTransactions.json();
 }
 
-export async function storeClaimTransaction(walletAddress: string, transactionId: string, solReceived: number, accountsClosed?: number) {
+export async function storeClaimTransaction(
+  walletAddress: string,
+  transactionId: string,
+  solReceived: number,
+  accountsClosed?: number
+) {
+  const claimTransaction: Record<string, string | number> = {
+    walletAddress,
+    transactionId,
+    solReceived,
+  };
 
-    const claimTransaction: Record<string, string | number> = {
-        walletAddress, 
-        transactionId, 
-        solReceived
-    }
+  if (accountsClosed) claimTransaction["accountsClosed"] = accountsClosed;
 
-    if(accountsClosed)
-        claimTransaction["accountsClosed"] = accountsClosed
-    
-    const url = `${API_URL}/api/claim-transactions/store`
-    const request = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(claimTransaction)
-    })
+  const url = `${API_URL}api/claim-transactions/store`;
+  const request = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(claimTransaction),
+  });
 
-    if(!request.ok)
-        throw new Error("An error occourred during server claim transaction storing")
+  if (!request.ok)
+    throw new Error(
+      "An error occourred during server claim transaction storing"
+    );
 
-    return await request.json()
+  return await request.json();
 }
